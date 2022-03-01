6586f4c0d81e3834fa45c1709db56764c2bfbba2
hateganfloringeorge <hateganfloringeorge@gmail.com>
2022-03-01 02:40:27 +0200

    Enemy AI with A*

---
+++ Game Sandbox/Assets/EnemyAI.cs
@@ -0,0 +1,79 @@
+using Pathfinding;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 200f;
+    public float nextWayPointDistance = 1.2f;
+
+    Path path;
+    int currentWayPoint = 0;
+    bool reachedEndOfPath = false;
+
+    public Transform EnemyGFX;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (path == null)
+            return;
+
+        if (currentWayPoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
+
+        if (distance < nextWayPointDistance)
+        {
+            currentWayPoint++;
+        }
+
+        if (force.x >= 0.01f)
+        {
+            EnemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (force.x <= -0.01f)
+        {
+            EnemyGFX.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
